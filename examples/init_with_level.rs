c929510651c88c7f72b068fb21f52a62c6cf002f
Andrey Kutejko <andy128k@gmail.com>
2018-02-04 18:28:21 +0100

    bump log dependency to 0.4.1

--- examples/init_with_level.rs
+++ examples/init_with_level.rs
@@ -2,10 +2,10 @@
 extern crate log;
 extern crate simple_logger;
 
-use log::LogLevel;
+use log::Level;
 
 fn main() {
-    simple_logger::init_with_level(LogLevel::Warn).unwrap();
+    simple_logger::init_with_level(Level::Warn).unwrap();
 
     warn!("This will be logged.");
     info!("This will NOT be logged.");
