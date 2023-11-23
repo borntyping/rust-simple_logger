2513891e2b020e7f3816ff624ab9fd0fdfb5eb6f
Sam Clements <sam@borntyping.co.uk>
2023-11-23 14:37:35 +0000

    Add an example of wrapping the logger

---
+++ examples/wrap.rs
@@ -0,0 +1,30 @@
+use simple_logger::{SimpleLogger, set_up_color_terminal};
+use log::{Log, Metadata, Record};
+
+struct WrapperLogger {
+    simple_logger: SimpleLogger
+}
+
+impl Log for WrapperLogger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        self.simple_logger.enabled(metadata)
+    }
+
+    fn log(&self, record: &Record) {
+        self.simple_logger.log(record)
+    }
+
+    fn flush(&self) {
+        self.simple_logger.flush()
+    }
+}
+
+fn main() {
+    set_up_color_terminal();
+
+    let simple_logger = SimpleLogger::new();
+    log::set_max_level(simple_logger.max_level());
+
+    let wrapper_logger = WrapperLogger { simple_logger };
+    log::set_boxed_logger(Box::new(wrapper_logger)).unwrap();
+}
